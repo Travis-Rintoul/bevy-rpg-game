b1f13896b368b472ec8fe1467a4b4f663c4c2ca6
Travis Rintoul <travis.rintoul@gmail.com>
2025-03-29 01:55:28 +1000

    MAIN: refactored enemy_plugin

--- src/plugins/enemy_plugin.rs
+++ src/plugins/enemy_plugin.rs
@@ -1,21 +1,7 @@
-use bevy::{app::{App, Plugin, Startup}, ecs::system::Res};
-
-use crate::{models::structs::enemy_definition::EnemyDefinitionCache, systems::enemy::enemy_import::load_enemy_data};
+use bevy::app::{App, Plugin};
 
 pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
-    fn build(&self, app: &mut App) 
-    {
-        app
-            .insert_resource(EnemyDefinitionCache::new())
-            .add_systems(Startup, (load_enemy_data, test_load));
-    }
-}
-
-fn test_load(res: Res<EnemyDefinitionCache>) {
-    match res.find(2) {
-        Some(enemy) => println!("{:?}", enemy),
-        _ => println!("not found")
-    }
+    fn build(&self, app: &mut App) {}
 }
